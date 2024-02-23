8d63494bb2e859d5f7b1b84ebc4da150fbfc4b7e
cam <ayresc17@holyportcollege.org.uk>
2024-02-23 10:49:50 +0000

    Added main, and made printing of worlds pretty.

--- src/Main.py
+++ src/Main.py
@@ -1,1 +1,8 @@
-#import pygame
+from WorldGen import *
+from People import *
+
+
+if(__name__ == "__main__"):
+    theWorld = World(0, sizeX = 12, sizeY = 16)
+    theWorld.buildWorld()
+    print(theWorld)
