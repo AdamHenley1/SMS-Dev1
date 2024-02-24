0d3e61fdbf27bcbd9d805456859681253494694c
cam <ayresc17@holyportcollege.org.uk>
2024-02-24 13:45:49 +0000

    Fixed Main's issue that prevented it from running.

--- src/Main.py
+++ src/Main.py
@@ -3,13 +3,11 @@ from People import *
 
 
 def startMenu():
-
-    
     print("Welcome to HAC society!")
     newW = input("Would you like to load a saved world? [y / N] ")
-    newWorld = World(0)
-    
+
     if(newW.lower() == 'y'):
+        newWorld = World(0)
         saveFile = input("What is the path to the save file: ")
         
         try:
@@ -18,17 +16,17 @@ def startMenu():
             print("Starting world...")
             return(newWorld)
         except:
-            print("ERROR: File could not be found. Exiting...")
+            raise Exception("File could not be found. Exiting...")
 
     else:
+        x,y = input("How big do you want the world to be [x y]: ").split(" ")
+        newWorld = World(0, sizeX = int(x), sizeY = int(y))
         print("Generating a new world...")
+
         newWorld.buildWorld()
         print("Success!")
 
         people = input("How many people do you want: ")
-        
-        x,y = input("How big do you want the world to be [x y]: ").split(" ")
-        newWorld = World(0, sizeX = int(x), sizeY = int(y))
         
         print("Spawning people...")
         for i in range(int(people)):
@@ -39,6 +37,5 @@ def startMenu():
 
 
 if(__name__ == "__main__"):
-#    world = startMenu()
-    newWorld = World(0, sizeX = 16, sizeY = 16)
-    print(newWorld)
+    world = startMenu()
+    print(world)
--- src/People.py
+++ src/People.py
@@ -144,61 +144,17 @@ class People:
             
         elif(radint(0, 10) < 6):
             self.findWood(self)
-        
-
-def startMenu():
-
-    
-    print("Welcome to HAC society!")
-    newW = input("Would you like to load a saved world? [y / N] ")
-    newWorld = World(0)
-    
-    if(newW.lower() == 'y'):
-        saveFile = input("What is the path to the save file: ")
-        
-        try:
-            newWorld.loadWorld(saveFile)
-            print("Success!")
-            print("Starting world...")
-            return(newWorld)
-        except:
-            print("ERROR: File could not be found. Exiting...")
-
-    else:
-        print("Generating a new world...")
-        newWorld.buildWorld()
-        print("Success!")
-
-        people = input("How many people do you want: ")
-        
-        x,y = input("How big do you want the world to be [x y]: ").split(" ")
-        newWorld = World(0, sizeX = int(x), sizeY = int(y))
-        
-        print("Spawning people...")
-        for i in range(int(people)):
-            spawn(newWorld)
-        
-        print("Starting world...")
-        return(newWorld)
 
 
         
 ## This will run if the file is directly called,
 ## but not if it is imported as a library.
 if(__name__ == "__main__"):
-    peopleNum = input("How many people do you want: ")
-    print("World is 64x16.")
-    andWorld = World(0, sizeX = 64, sizeY = 16)
+    peopleNum = int(input("How many people do you want: "))
+    print("World is 64x32.")
+    andWorld = World(0, sizeX = 64, sizeY = 32)
     andWorld.buildWorld()
-    for i in range(int(peopleNum)):
+    for i in range(peopleNum):
         spawn(andWorld)
+
     print(andWorld)
-    
-#    for i in range(population):
-#        temp = People(getName())
-#        people_population.append(temp)
-#        #def person(age = 0):
-#    for i in range(population):
-#        temp = People_population[i]
-#        print("Name:",temp.return_name(),", Job:",temp.return_job())            
-#
